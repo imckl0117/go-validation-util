e2fb1d00bc0b11760b218480c501663f116e0e16
Kok Leong Chan <kl.chan@hotmail.com>
2021-12-15 13:40:27 +0800

    Add package description

--- number.go
+++ number.go
@@ -1,3 +1,9 @@
+/*
+Package validation exposes some utility functions to validate strings and
+numbers. Validation error messages are formatted in the form of
+'fieldName,errorCode'. errorCode may contain additional information, such as
+min/max length, etc.
+*/
 package validation
 
 import (
